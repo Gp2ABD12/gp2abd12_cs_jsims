6f58c40350a09c7d14d344b29a2206b0fbade7ca
Abraham <Abraham@10.18.54.37>
2025-04-11 15:59:53 +0300

    field of study added to the backend

---
+++ csjsimscore/src/domain/FieldOfStudy.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author Abraham
+ */
+public class FieldOfStudy {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+    int id;
+    String name;
+    int departmentId;
+}
